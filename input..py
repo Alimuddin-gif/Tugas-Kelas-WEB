9867313f1a5ea24cf21e55288826977b77518a86
Alimuddin-gif <113223251+Alimuddin-gif@users.noreply.github.com>
2022-09-16 08:56:07 +0700

    Menambahkan

---
+++ input..py
@@ -0,0 +1,22 @@
+
+
+print("Nama Saya Alimuddin Nur Baweansyah", "Prodi SISTEM INFORMASI(220441100027)")
+print("Selamat Datang Di kalkulator Cerdas")
+print("Saatnya menghitung suhu")
+suhu= int(input("masukkan angka untuk dihitung :"))
+print(("hasil dari celcius ke farenhait :"),int(suhu*9)//5 + 32)
+print(("hasil dari celcius ke kelvin :"), int(suhu + 273))
+print("...........................")
+print("        SELANJUTNYA        ")
+print("...........................")
+print("Saatnya menghitung Luas Balok")
+panjang= int(input("Masukkang panjang :"))
+Lebar= int(input("masukkan lebar :"))
+Tinggi = int(input("maukkan tinggi :"))
+print(("luas balok adalah"),int(panjang*Tinggi*Lebar))
+print("arigato")
+
+
+
+
+
